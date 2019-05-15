8695954f5b930824330f69fb730be926f7513aaf
Anjali Verma <anjaliverma@Anjalis-MacBook-Pro.local>
2019-05-15 00:22:57 -0500

    changes to create databases

--- run.py
+++ run.py
@@ -5,7 +5,8 @@ logging.config.fileConfig("config/logging/local.conf")
 logger = logging.getLogger("run-whos-the-boss")
 
 from src.downloadData import load_data
-from config import BUCKET_NAME
+from src.add_schema import create_sqlite_db, create_rds_db
+from config import BUCKET_NAME, SQLALCHEMY_DATABASE_URI, DATABASE_NAME
 
 if __name__ == '__main__':
 
@@ -15,6 +16,16 @@ if __name__ == '__main__':
     sub_process = subparsers.add_parser('loadS3')
     sub_process.add_argument("--bucket", type=str, default=BUCKET_NAME, help="Bucket to be copied to")
     sub_process.set_defaults(func=load_data)
+
+    sub_process = subparsers.add_parser('createSqlite')
+    sub_process.add_argument("--engine_string", type=str, default=SQLALCHEMY_DATABASE_URI,
+                             help="Connection uri for SQLALCHEMY")
+    sub_process.set_defaults(func=create_sqlite_db)
+
+    sub_process = subparsers.add_parser('createRDS')
+    sub_process.add_argument("--database", type=str, default=DATABASE_NAME,
+                             help="Database in RDS")
+    sub_process.set_defaults(func=create_rds_db)
 
     args = parser.parse_args()
     args.func(args)
