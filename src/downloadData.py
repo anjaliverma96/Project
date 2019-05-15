6bcfa3a8fb3cac20b3749920603d4caa77c4f941
Anjali Verma <anjaliverma@dhcp-10-105-16-188.wireless.northwestern.private>
2019-05-14 19:33:59 -0500

    added and removed files

---
+++ src/downloadData.py
@@ -0,0 +1,13 @@
+import boto3
+
+def load_data(args):
+
+    s3 = boto3.resource('s3')
+
+    copy_source1 = {'Bucket': 'jokerecommender', 'Key': 'ratings.csv'}
+    copy_source2 = {'Bucket': 'jokerecommender', 'Key': 'jokes.csv'}
+
+    bucket = s3.Bucket('projectdatadownload')
+
+    bucket.copy(copy_source1, 'ratings.csv')
+    bucket.copy(copy_source2, 'jokes.csv')
